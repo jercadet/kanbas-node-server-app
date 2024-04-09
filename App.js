711a11d1ec65020d03c38c273a3724cc199e7033
cadetjer <tibullsilverblaze@gmail.com>
2024-04-09 15:20:29 -0400

    on 3.3 of a6

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
-import express from 'express';
+import express from "express";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import cors from "cors";
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/kanbas");
 const app = express();
 app.use(cors());
 app.use(express.json());
