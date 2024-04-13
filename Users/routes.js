003725058ebc10cefcd7f6b8412439b97a78758b
cadetjer <tibullsilverblaze@gmail.com>
2024-04-13 02:46:56 -0400

    fixed adding on User page

--- Users/routes.js
+++ Users/routes.js
@@ -1,7 +1,11 @@
 import * as dao from "./dao.js";
 
 export default function UserRoutes(app) {
-  const createUser = async (req, res) => {};
+    const createUser = async (req, res) => {
+        const user = await dao.createUser(req.body);
+        res.json(user);
+      };
+    
 
   const deleteUser = async (req, res) => {
     const status = await dao.deleteUser(req.params.userId);
